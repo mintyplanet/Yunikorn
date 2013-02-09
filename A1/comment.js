df05bf87c5fe2f4674ccc7ea82ad4e40c6921a41
Kevin <kevin@Cinnamon-VirtualBox.(none)>
2013-02-09 16:34:01 -0500

    changed models from prototypal OOP to classical OOP.

--- A1/comment.js
+++ A1/comment.js
@@ -1,41 +1,35 @@
 /*Defining a comment model
- * to create a comment variable: Object.create(topic, {commentList: {value : list}, {commentBody:{value: "string"}}});
- * to set or change the variales: var comment = Object.create(comment);
- *												comment.ID = 10;
- * to call the functions: var commentID = comment.getID()
  */
-var comment(){
-	// return an array of comments
-	getCommentsArray : function(){
-		return this.commentsArray;
-	}
+function Comment(body){
+	this.body = body;
+	this.comment = new Array();
+	this.upvote = 0;
+} 
+
+Comment.prototype = {
 
 	// return the body of the comment
-	getCommentBody : function(){
-		return this.commentBody;
-	}
+	getBody : function(){
+		return this.body;
+	},
 
-	// return commentID
-	getID: function(){
-		return this.ID;
-	}
+	// return an array of comments
+	getComments : function(){
+		return this.comment;
+	},
 
 	//return the upvote of the comment
 	getupvote : function(){
 		return this.upvote;
-	}
+	},
 
-	//return the number of upvote its subcomments has
-	getsubupvote: function(){
-		var upvoteCount = 0;
-		var comment;
-		for (var i = 0; i < this.commentsArray.length; i++){
-			comment = this.commentsArray[i];
-			upvoteCount += comment.getupvote;
-			upvoteCount += comment.getsubupvote; 
-		}
-		return upvoteCount;
+	voteup: function(){
+		this.upvote++;
+	},
+
+	addComment: function(commentID){
+		this.comment.push(commentID);
 	}
 }
 
-module.exports = comment;
+module.exports = Comment;
