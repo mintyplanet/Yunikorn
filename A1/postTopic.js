d69262eec63e6f516e842910a655303a8e9b7365
Karin Ng <karinng10@gmail.com>
2013-02-12 16:07:33 -0500

    refactored the creation of posts to be much more efficient and cleaner. Still have javascript issue with the replies

--- A1/postTopic.js
+++ A1/postTopic.js
@@ -1,38 +1,18 @@
 $(document).ready(function(){
-
-	var topHTML = '<div class="topic"> \
-      <div class="topic_head"> \
-      	<h3> \
-      	<a href="javascript:void(0)">';
-		
-	var topHTML2 = '</a> \
-      	<span class="topic_url"><a href="javascript:void(0)"><small>(topic.url)</small></a></span> \
-      	</h3> \
-      </div> \
-      <div class="topic_meta"><small>Post Info | <span class="num_comments"></span></small> \
-		<div class="reply"> \
-		<a href="javascript:void(0)"><small>reply</small></a> \
-			<div class="reply_body"> \
-				<textarea class="reply_form" rows="5"></textarea><br /> \
-				<button class="btn btn-small" type="button">Submit</button> \
-			</div> \
-		</div> \
-	  </div> \
-	  <div class="reply"> \
-			<div class="reply_body"> \
-				<textarea class="reply_form" rows="5"></textarea><br /> \
-				<button class="btn btn-small" type="button">Submit</button> \
-			</div> \
-	  </div> \
-	  </div>'; 
-	  
 	$("button.btn.btn-post").click(function(){
 		$.post("/topic", {
 					title:$(".topic_title_form").val(),
 					link:$(".topic_link_form").val()
 				}, function(data, status) {
 					alert("success " + data.topicID);
-					$(".postTopic").after(topHTML + data.title + topHTML2);
+					
+					// Add a topic
+					$("#testTop").clone().attr({
+						id:data.topicID,
+						style:"display: inline"}).appendTo("#copy-contain");
+						
+					// Change topic information
+					$("#"+data.topicID).children("div.topic_head").children("h3").children("a").html(data.title);
 				}).fail(function() { alert("error"); });
 	});
 });
