f20450f90d8572a6216d3cf9ba6b4c5f06e9e418
Yuki Saito <mintyplanet@gmail.com>
2013-02-12 22:31:25 -0500

    Made timestamp look better

--- A1/submitComment.js
+++ A1/submitComment.js
@@ -15,7 +15,8 @@ $(document).ready(function(){
 					style:"display: inline"}).appendTo("#" + id);
 						
 				// Change topic information
-				$("#"+data.commentID).children("div.comment_meta").children("small").html(data.timestamp);
+				$("#"+data.commentID).children("div.comment_meta").children("small").html(
+				new Date(data.timestamp).toLocaleString());
 				$("#"+data.commentID).children("p").html(data.body);
 			}).fail(function() { alert("error"); });
   });
