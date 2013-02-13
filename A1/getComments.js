8d888021407d1880e19b9a2990cc6b8bdf4c7148
Karin Ng <karinng10@gmail.com>
2013-02-13 16:31:07 -0500

    Fixed comment bug, and toggle replies

--- A1/getComments.js
+++ A1/getComments.js
@@ -1,34 +1,6 @@
 $(document).ready(function(){
-var commentTemplate = $('#testCom');
-function commentDiv(comment){
-		var cmnt = commentTemplate.clone();
-		cmnt.attr({id:comment.commentID, style:"display: inline"});
-		cmnt.find('.timestamp').html(new Date(comment.timestamp).toLocaleString());
-		cmnt.find("#bodyText").html(comment.body);
-		cmnt.children(".comment_meta").children(".voteNum").html(comment.upvote); // MUST keep this as children (can't use find) or it will screw up child comment votes
-		$.each(comment.comment, function(i, nested) {
-			cmnt.append(commentDiv(nested));
-			//commentDiv(nested).appendTo(cmnt);
-		});
-		//console.log(cmnt.html());
-		return cmnt;
-}
-
-	$(document).on ('click', '.topic_head', function(){
-		var topId = $(this).closest('.topic').attr('id');
-		// Get top level comments
-		$.getJSON("/topic/" + topId + "/comment", function(data, status) {
-			var topicComments = data["comments"];
-			
-			//console.log(topicComments);
-			$.each(topicComments, function(i, comment){
-				//console.log(comment);
-				var comments = commentDiv(comment);
-				//console.log(topId);
-				//comments.appendTo("#" + topId);
-				$("#"+topId).append(comments);
-			});
-		});
+	$(document).on('click', '.topic_head', function () {
+		$(this).siblings('.comment').toggle();
+		$(this).siblings('.comment').find('.comment').toggle();
 	});
-});
-
+}); 
