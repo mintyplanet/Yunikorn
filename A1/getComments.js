f1ec48d4c6f16b5d1e4a05039b26a5e74f894b98
Kevin <kevin@kevin-VirtualBox.(none)>
2013-02-13 20:48:14 -0500

    changed it so the title now toggles comments and only get comments when it is hidden.

--- A1/getComments.js
+++ A1/getComments.js
@@ -1,6 +1,36 @@
 $(document).ready(function(){
+	
+	var commentTemplate = $('#testCom');
 	var shown = false;
 
+	function toggleComm(topic, shown)
+	{
+		topic.siblings('.comment').toggle();
+		topic.siblings('.comment').find('.comment').toggle();
+	
+		return !shown;
+	}
+
+
+	function commentDiv(comment){
+		var cmnt = commentTemplate.clone();
+		cmnt.attr({id:comment.commentID, style:"display: inline"});
+		// if the comment is already there do not proceed, else grab the comment.
+		if ($("#"+ comment.commentID + ".comment").length != 0){
+			return "";
+		}
+
+		cmnt.find('.timestamp').html(new Date(comment.timestamp).toLocaleString());
+		cmnt.find("#bodyText").html(comment.body);
+		cmnt.children(".comment_meta").children(".voteNum").html(comment.upvote); // MUST keep this as children (can't use find) or it will screw up child comment votes
+		$.each(comment.comment, function(i, nested) {
+			cmnt.append(commentDiv(nested));
+			//commentDiv(nested).appendTo(cmnt);
+		});
+		//console.log(cmnt.html());
+		return cmnt;
+	}
+	
 	// If click the submit button and not currently showing comments, show them
 	$(document).on('click', 'button.btn-small.com', function () {
 		var top = $(this).closest('.topic').find(".topic_head");
@@ -10,17 +40,26 @@ $(document).ready(function(){
 			shown = toggleComm(top, shown);
 		}
 	});
-	
-	// If click the topic head, show rest of the comments
-	$(document).on('click', '.topic_head', function () {
+
+	//if the title is clicked
+	$(document).on ('click', '.topic_head', function(){
+		//only retrieve comments if the comments are not shown
+		if (!shown){
+			var topId = $(this).closest('.topic').attr('id');
+			// Get top level comments
+			$.getJSON("/topic/" + topId + "/comment", function(data, status) {
+				var topicComments = data["comments"];
+
+				//console.log(topicComments);	
+				$.each(topicComments, function(i, comment){
+					//console.log(comment);
+					var comments = commentDiv(comment);
+					//console.log(topId);
+					//comments.appendTo("#" + topId);
+					$("#"+topId).append(comments);
+				});
+			});
+		}
 		shown = toggleComm($(this), shown);
 	});
-}); 
-
-function toggleComm(topic, shown)
-{
-	topic.siblings('.comment').toggle();
-	topic.siblings('.comment').find('.comment').toggle();
-	
-	return !shown;
-}
+});
