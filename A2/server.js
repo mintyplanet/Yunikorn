f1b851d1955be0bb4a802a58e2180202f67551d3
Yuki Saito <mintyplanet@gmail.com>
2013-02-25 04:57:07 -0500

    Tumblr API works for the most part

--- A2/server.js
+++ A2/server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
-	tumblrAPI = require('./tumblr'),
+	Tumblr = require('./tumblr'),
+	util = require('util'),
 	app = express(),
-	tumblr = tumblrAPI('EzNnvqdhs5XPSAAm7ioYyxXgyFQHlIDYtqYhifb3oi5fqkQl69');
+	tumblr = new Tumblr('EzNnvqdhs5XPSAAm7ioYyxXgyFQHlIDYtqYhifb3oi5fqkQl69');
 
 /* 
  * OAuth Consumer Key: EzNnvqdhs5XPSAAm7ioYyxXgyFQHlIDYtqYhifb3oi5fqkQl69
@@ -14,9 +15,13 @@ app.use(express.logger('dev'));
 
 /* Routes */
 app.post('/blog', function(req, res){
-	res.json(tumblr);
+	//TODO: Get the base hostname as a parameter. Save it in the db for tracking.
+	res.json({"status": 200,"msg": "OK"});
 });
 app.get('/blog/:blogname/trends', function(req, res) {
+	//TODO: Well, basically everything
+	
+	// getting the parameters is already done.
 	var order = req.query.order,
 		limit = req.query.limit,
 		blogname = req.params.blogname;
@@ -24,10 +29,19 @@ app.get('/blog/:blogname/trends', function(req, res) {
 	res.json({"trending": [{"not-a-real-field":blogname}], "order": order, "limit": limit});
 });
 app.get('/blogs/trends', function(req, res) {
+	//TODO: Well, basically everything.
 	var order = req.query.order,
 		limit = req.query.limit;
 	res.json({"trending": [], "order": order, "limit": limit});
 });
+
+setInterval(function(){
+	util.log('This is scheduled to run every 5 seconds');
+}, 1000*5 );
+
+setInterval(function(){
+	util.log('And this one, every hour. Maybe we can use this to schedule the hourly tracking');
+}, 1000*60*60 );
 
 /* Sit back and listen to port */
 app.listen(PORT);
