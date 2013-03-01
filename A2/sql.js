3cf4fa6353872b2632c9775891da1a923c33b299
Kevin <kevin@kevin-VirtualBox.(none)>
2013-02-28 22:18:33 -0500

    changed some stuff in sql and server.js.

--- A2/sql.js
+++ A2/sql.js
@@ -5,29 +5,29 @@ var db = new sqlite3.Database(':memory:');
 
 /* creat tables with the following schema:
  * post(postID(Key), url, text, image, date)
- * tracking(hostName(Key), postid(Key & Foreign Key), Sequence(Key), count)
+ * tracking(hostName(Key), postid(Key & Foreign Key), Sequence(Key), count, time)
  */
 db.run("CREATE TABLE IF NOT EXISTS post(postID int NOT NULL, url varchar(255) NOT NULL,\
 	 text varchar(255), image varchar(255), date varchar(20) NOT NULL, PRIMARY KEY (postID))", 
 	function(err, row) { (err)? console.log(err) : console.log("created table post");});
 
 db.run("CREATE TABLE IF NOT EXISTS tracking(hostName varchar(255) NOT NULL, postID int \
-	NOT NULL, sequence int NOT NULL, count int NOT NULL, \
+	NOT NULL, sequence int NOT NULL, count int NOT NULL, time varchar(20), \
 	PRIMARY KEY(hostName, postID, sequence), FOREIGN KEY (postID) REFERENCES post(postID))",
 	function(err, row) { (err)? console.log(err) : console.log("created table tracking");});
 
 /*
  * Handle insertion of each blog post for getBlog.
  */
-exports.insertGetBlog = function(postID, url, text, image, date, baseHostName, postID, sequence, count){
+exports.insertGetBlog = function(postID, url, text, image, date, baseHostName, sequence, count){
 	db.serialize(function(){
 		db.run("INSERT INTO post VALUES (?,?,?,?,?)", [postID, url, text, image, date], 
 			function(err, row) { (err)? 
-			console.log(err) : console.log("inserted " + postID + "into post");});
+			console.log(err) : console.log("inserted " + postID + " into post");});
 
-		db.run("INSERT INTO tracking VALUES (?,?,?,?)", [baseHostName, postID, sequence, count],
+		db.run("INSERT INTO tracking VALUES (?,?,?,?)", [baseHostName, postID, sequence, count, date],
 			function(err, row) { (err)? 
-			console.log(err) : console.log("inserted " + postID + "into tracking");});
+			console.log(err) : console.log("inserted " + postID + " into tracking");});
 	});
 }
 
@@ -36,8 +36,24 @@ exports.insertGetBlog = function(postID, url, text, image, date, baseHostName, p
  */
 exports.checkBlog = function(blogName){
 	var exist;
-	db.get("SELECT hostName FROM tracking WHERE hostName = ?", baseHostName, 
-		function(err, row) { (err)? console.log(err) : console.log("querying error in storeBlog");
+	db.get("SELECT hostName FROM tracking WHERE hostName = ?", blogName, 
+		function(err, row) { (err)? console.log(err) : console.log("CheckBlog sucessful");
 			(row)? exist = true : exist = false;});
 	return exist;
+}
+
+exports.showTables = function(){
+	db.serialize(function(){
+		db.each("SELECT * FROM post", 
+			function(err, row) { (err)? console.log(err) : console.log("Showing table post");
+				// need to go thru each row
+				console.log(row);
+		});
+
+		db.each("SELECT * FROM tracking", 
+			function(err, row) { (err)? console.log(err) : console.log("Showing table tracking");
+				// need to go thru each row
+				console.log(row);
+		});
+	});
 }
