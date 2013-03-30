8616b6d8a07e74bb52692635d820cd0e477a6d6f
naobot <cui.naomi@gmail.com>
2013-03-29 23:04:25 -0400

    -in progress of getting wide-view layout buttons in place

--- A3/index.js
+++ A3/index.js
@@ -1,20 +1,37 @@
-$(document).load(function(){
+$(document).ready(function(){
 	var width = $(window).width();
+	var height = $(document).height();
+	modHeight = height - 100;
 	widthDiff = 700;
+	
 	if (width < widthDiff) {
 		$("#main_content").width("95%");
 		$(".narrowview").show();
-		$(".wideview").hide();
+		$(".wideview").hide();		
 	} else {
-		$("#main_content").width("70%").addClass("wideview");
+		$("#main_content").width("70%");
 		$(".wideview").show();
 		$(".narrowview").hide();
+		$(".left_arrow").position({
+			my: "right",
+			at: "left",
+			of: ".tweet_content"
+		});
+		$(".right_arrow").position({
+			my: "left",
+			at: "right",
+			of: ".tweet_content"
+		});
 	}
+	
+	$("#main_content").height(modHeight);
 });
 
 
 $(window).resize(function(){
 	var width = $(window).width();
+	var height = $(document).height();
+	modHeight = height - 100;
 	widthDiff = 700;
 	if (width < widthDiff) {
 		$("#main_content").width("95%");
@@ -25,4 +42,6 @@ $(window).resize(function(){
 		$(".wideview").show();
 		$(".narrowview").hide();
 	}
+	
+	$("#main_content").height(modHeight);
 });
